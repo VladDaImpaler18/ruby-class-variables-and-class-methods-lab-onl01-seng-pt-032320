9a5dbd43fde9667ea8af3f15e1244fe3e0f01524
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-21 09:06:40 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class Song
+  
+  attr_accessor :artist, :genre, :name
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(songName, songArtist, songGenre)
+    @name = songName
+    @artist = songArtist
+    @genre = songGenre
+    @@artists << songArtist if !@@artists.include?(songArtist)
+    @@genres << songGenre if !@@genres.include?(songGenre)
+	  @@count+=1
+  end
+  #binding.pry
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    @@artists
+  end
+
+  def self.genre_count
+    hex = {}
+	  @genre.each { |genre| hex.key?(genre) ? hex[genre] += 1 : hex[genre] = 1 }
+	  hex
+  end
+  
+  def self.artist_count
+    @@artists.length
+    #hex = {}
+	  #@artists.each { |artist| hex.key?(artist) ? hex[artist] +=1 : hex[artist] = 1 }
+	  #hex
+  end
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.genre_count
+    binding.pry
+  end
+  
+end
