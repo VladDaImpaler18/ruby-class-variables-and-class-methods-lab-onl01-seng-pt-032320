5b1c3c813a7298e9b441880039bd116fa55bc415
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-25 07:53:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,16 +26,11 @@ class Song
   end
 
   def self.genre_count
-    hex = {}
-	  @@genre.each { |genre| hex.key?(genre) ? hex[genre] += 1 : hex[genre] = 1 }
-	  hex
+    binding.pry
   end
   
   def self.artist_count
-    hex = {}
-    binding.pry
-	  @@artists.each { |artist| hex.key?(artist) ? hex[artist] +=1 : hex[artist] = 1 }
-	  hex
+    
   end
   
   def self.count
